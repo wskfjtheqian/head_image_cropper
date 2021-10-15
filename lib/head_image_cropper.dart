2db297ba55b1c08f1e73c413c04eab42b2405ced
heqian <182359041@qq.com>
2021-10-15 18:04:25 +0800

    添加加载错误回调

--- lib/head_image_cropper.dart
+++ lib/head_image_cropper.dart
@@ -4,8 +4,7 @@ import 'dart:async';
 import 'dart:math' as math;
 import 'dart:ui';
 import 'dart:ui' as ui show Image;
-import 'src/cropper_image_out.dart'
-    if (dart.library.html) 'src/cropper_image_web_out.dart' as imgOut;
+import 'src/cropper_image_out.dart' if (dart.library.html) 'src/cropper_image_web_out.dart' as imgOut;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -39,6 +38,7 @@ class CropperImage extends RenderObjectWidget {
     this.outHeight = 256.0,
     this.maskPadding = 20.0,
     this.round = 8.0,
+    this.onLoadError,
   }) : super(key: key);
 
   ///  image 输入图片源
@@ -80,7 +80,11 @@ class CropperImage extends RenderObjectWidget {
   ///round 预览框圆角 默认值：8
   final double round;
 
+  ///控制器
   final CropperController? controller;
+
+  ///加载出错的回调
+  final void Function(BuildContext context, Object exception, StackTrace? stackTrace)? onLoadError;
 
   @override
   CropperImageElement createElement() {
@@ -105,8 +109,7 @@ class CropperImage extends RenderObjectWidget {
   }
 
   @override
-  void updateRenderObject(
-      BuildContext context, CropperImageRender renderObject) {
+  void updateRenderObject(BuildContext context, CropperImageRender renderObject) {
     renderObject
       ..limitations = limitations
       ..isArc = isArc
@@ -148,28 +151,27 @@ class CropperImageElement extends RenderObjectElement {
   CropperImageElement(CropperImage widget) : super(widget);
 
   @override
-  CropperImageRender get renderObject =>
-      super.renderObject as CropperImageRender;
+  CropperImageRender get renderObject => super.renderObject as CropperImageRender;
 
   @override
   CropperImage get widget => super.widget as CropperImage;
 
   @override
   void forgetChild(Element child) {
-   super.forgetChild(child);
+    super.forgetChild(child);
   }
 
   void _resolveImage() {
     if (null == _image) {
       return;
     }
-    final ImageStream stream =
-        _image!.resolve(createLocalImageConfiguration(this));
+    final ImageStream stream = _image!.resolve(createLocalImageConfiguration(this));
     var listener;
     listener = ImageStreamListener((image, synchronousCall) {
       renderObject.image = image.image;
       stream.removeListener(listener);
     }, onError: (exception, stackTrace) {
+      widget.onLoadError?.call(this, exception, stackTrace);
       stream.removeListener(listener);
     });
     stream.addListener(listener);
@@ -305,28 +307,22 @@ class CropperImageRender extends RenderProxyBox {
 
   void handleDownEvent(PointerDownEvent event) {
     if (null == _old1 && _old2?.device != event.device) {
-      _old1 = Pointer(
-          device: event.device, dx: event.position.dx, dy: event.position.dy);
+      _old1 = Pointer(device: event.device, dx: event.position.dx, dy: event.position.dy);
     } else if (null == _old2 && _old1!.device != event.device) {
-      _old2 = Pointer(
-          device: event.device, dx: event.position.dx, dy: event.position.dy);
+      _old2 = Pointer(device: event.device, dx: event.position.dx, dy: event.position.dy);
     }
   }
 
   void handleMoveEvent(PointerMoveEvent event) {
     if (_old1?.device == event.device) {
-      _new1 = Pointer(
-          device: event.device, dx: event.position.dx, dy: event.position.dy);
+      _new1 = Pointer(device: event.device, dx: event.position.dx, dy: event.position.dy);
     } else if (_old2?.device == event.device) {
-      _new2 = Pointer(
-          device: event.device, dx: event.position.dx, dy: event.position.dy);
+      _new2 = Pointer(device: event.device, dx: event.position.dx, dy: event.position.dy);
     }
 
     if (null != _old1 && null != _old2 && null != _new1 && null != _new2) {
-      var newLine = math.sqrt(
-          math.pow(_new1!.dx! - _new2!.dx!, 2) + math.pow(_new1!.dy! - _new2!.dy!, 2));
-      var oldLine = math.sqrt(
-          math.pow(_old1!.dx! - _old2!.dx!, 2) + math.pow(_old1!.dy! - _old2!.dy!, 2));
+      var newLine = math.sqrt(math.pow(_new1!.dx! - _new2!.dx!, 2) + math.pow(_new1!.dy! - _new2!.dy!, 2));
+      var oldLine = math.sqrt(math.pow(_old1!.dx! - _old2!.dx!, 2) + math.pow(_old1!.dy! - _old2!.dy!, 2));
       this.scale *= (newLine / oldLine);
 
       this.drawX += ((_new1!.dx! - _old1!.dx!) + (_new2!.dx! - _old2!.dx!)) / 2;
@@ -342,8 +338,7 @@ class CropperImageRender extends RenderProxyBox {
         }
       }
       markNeedsPaint();
-    } else if ((null != _old1 && null != _new1) ||
-        (null != _old2 && null != _new2)) {
+    } else if ((null != _old1 && null != _new1) || (null != _old2 && null != _new2)) {
       this.drawX += ((_new1 ?? _new2)!.dx! - (_old1 ?? _old2)!.dx!);
       this.drawY += ((_new1 ?? _new2)!.dy! - (_old1 ?? _old2)!.dy!);
       markNeedsPaint();
@@ -399,8 +394,7 @@ class CropperImageRender extends RenderProxyBox {
       canvas.translate(centerX! + drawX, centerY! + drawY);
       canvas.rotate(rotate1);
       canvas.scale(scale);
-      canvas.drawImage(
-          _image!, Offset(-_image!.width/ 2, -_image!.height/ 2), Paint());
+      canvas.drawImage(_image!, Offset(-_image!.width / 2, -_image!.height / 2), Paint());
       canvas.restore();
     }
 
@@ -414,10 +408,7 @@ class CropperImageRender extends RenderProxyBox {
 
       for (double x = 0; x < size.width; x += backBoxSize) {
         canvas.drawRect(
-            Rect.fromLTRB(x, y, x + backBoxSize, y + backBoxSize),
-            Paint()
-              ..color = (color =
-                  color == backBoxColor1 ? backBoxColor0 : backBoxColor1));
+            Rect.fromLTRB(x, y, x + backBoxSize, y + backBoxSize), Paint()..color = (color = color == backBoxColor1 ? backBoxColor0 : backBoxColor1));
       }
     }
   }
